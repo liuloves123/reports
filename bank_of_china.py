a292c691ea3be4e5ae17d934a652c7428cb9ea49
watermelon <liuwei_0524@aliyun.com>
2022-02-22 17:02:55 +0800

    完成pdf链接爬取

--- bank_of_china.py
+++ bank_of_china.py
@@ -28,11 +28,33 @@ def get_url(html):
     soup = BeautifulSoup(html, 'lxml')
     list_url = soup.select('body > div > div.main > div > ul>li')
     for li in list_url:
-        url = li.find('a').attrs['href']
+        url = li.find('a').attrs['href'].replace('.', '', 1)
+        url = 'https://www.boc.cn/investor/ir3' + url
         total_url.append(url)
+    return total_url
+
+
+def get_pdf(total_url):
+    """
+    获取pdf链接
+    :return:
+    """
+    total_pdf = {}
+    for url in total_url:
+        html = get_html(url)
+        soup = BeautifulSoup(html, 'lxml')
+        list_pdf = soup.select('body > div > div.container > div.content.con_area > div.sub_con>div>ul>li')
+        for li in list_pdf:
+            pdf = li.find('a').attrs['href']
+            title = li.find('a').string
+            print(pdf, title)
+            total_pdf[title] = pdf
 
 
 if __name__ == "__main__":
-    url = r'https://www.boc.cn/investor/ir3/'
-    html = get_html(url)
-    get_url(html)
+    url = r'https://www.boc.cn/investor/ir3/index.html'
+    for i in range(1,2):
+        html = get_html(url)
+        total_url = get_url(html)
+        get_pdf(total_url)
+        url = f'https://www.boc.cn/investor/ir3/index_{i}.html'
